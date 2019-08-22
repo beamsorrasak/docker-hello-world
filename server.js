15e5108c18dd736945ac595251d8f325551a14b3
Sorrasak Phonklad <beam@flowaccount.com>
2019-08-22 16:25:15 +0700

    env for port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.HTTP_PORT || 3000
 
 app.get('/', (req, res) => res.send(`Hello World! ${process.env.SHOW_TEXT ? process.env.SHOW_TEXT : 'guest'}`))
 
