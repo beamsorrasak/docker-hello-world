08c17338eabda2303334e23197444b86f71d4394
Sorrasak Phonklad <beam@flowaccount.com>
2019-08-22 16:46:15 +0700

    config js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.HTTP_PORT || 3000
+const config = require('./config')
+const port = 3000
 
 app.get('/', (req, res) => res.send(`Hello World! ${process.env.SHOW_TEXT ? process.env.SHOW_TEXT : 'guest'}`))
+app.get('/config', (req, res) => res.send(config.key))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
